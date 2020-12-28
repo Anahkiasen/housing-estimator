6730a59ccccaf617cd858bcb6f40f2a47bc40dc8
Emma FABRE <ehtnam6@gmail.com>
2020-12-28 11:56:19 +0100

    Refactor to use helpers

--- src/train.py
+++ src/train.py
@@ -1,166 +1,106 @@
-from learntools.core import binder
-from learntools.ml_intermediate.ex4 import *
-from sklearn.compose import ColumnTransformer, make_column_transformer
-from sklearn.ensemble import RandomForestRegressor
+# %% [code]
+import pandas as pd
+import seaborn as sns
+from sklearn.compose import make_column_transformer
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from xgboost import XGBRegressor
-import matplotlib.pyplot as plt
-import numpy as np
-import optparse
-import os
-import pandas as pd
-import time
+from helpers import *
 
-if not os.path.exists("../input/train.csv"):
-    os.symlink("../input/home-data-for-ml-course/train.csv", "../input/train.csv")
-    os.symlink("../input/home-data-for-ml-course/test.csv", "../input/test.csv")
+plt.rcParams.update({"figure.figsize": (13, 10)})
+# %% [code]
+# Import the data
+X_train = pd.read_csv("../input/home-data-for-ml-course/train.csv", index_col="Id")
+X_test = pd.read_csv("../input/home-data-for-ml-course/test.csv", index_col="Id")
 
-binder.bind(globals())
-print("Setup Complete")
+# %% [code]
+## Split predictor
+[X, y] = split_predictor(X_train, "SalePrice")
 
-# 1. Prepare the data
-########################################
+# %%
+# Draw correlations
+correlations = X_train.corr()
 
-# Read the data
-X_train_full = pd.read_csv("../input/train.csv", index_col="Id")
-X_test_full = pd.read_csv("../input/test.csv", index_col="Id")
+sns.heatmap(correlations, mask=correlations < 0.8)
+# %% [code]
+## Define data leaks
+data_leaks = ["MoSold", "YrSold", "SaleType"]
 
-# Remove rows with missing target
-X_train_full.dropna(axis=0, subset=["SalePrice"], inplace=True)
+## Define arbitrary features
+arbitrary = []
 
-# Separate target from predictors
-y = X_train_full.SalePrice
-X_train_full.drop(["SalePrice"], axis=1, inplace=True)
-
-# Remove data leakage
-potential_leaks = ["MoSold", "YrSold", "SaleType"]
-X_train_full = X_train_full.drop(potential_leaks, axis=1)
-
-# "Cardinality" means the number of unique values in a column
-# Select categorical columns with relatively low cardinality (convenient but arbitrary)
-cardinal_categorical_cols = [
-    cname
-    for cname in X_train_full.columns
-    if X_train_full[cname].nunique() > 0 and X_train_full[cname].dtype == "object"
+## Define correlated features
+correlated = [
+    "GarageYrBlt",
+    # "TotRmsAbvGrd",
+    # "GarageCars",
+    #  "1stFlrSF"
 ]
 
-# Select numerical columns
-numerical_cols = [
-    cname
-    for cname in X_train_full.columns
-    if X_train_full[cname].dtype in ["int64", "float64"]
-]
-
-# Keep selected columns only
-selected_cols = cardinal_categorical_cols + numerical_cols
-excluded_cols = [col for col in X_train_full.columns if col not in selected_cols]
-X = X_train_full[selected_cols].copy()
-X_test = X_test_full[selected_cols].copy()
-
-
-# 2. Create prediction pipeline
-########################################
-
-# Preprocessing for numerical data
-numerical_transformer = SimpleImputer(strategy="constant")
-
-# Preprocessing for categorical data
-categorical_transformer = Pipeline(
-    steps=[
-        ("imputer", SimpleImputer(strategy="most_frequent")),
-        ("onehot", OneHotEncoder(handle_unknown="ignore")),
-    ]
+## Select features
+[continuous, categorical] = get_features(
+    X,
+    data_leaks + arbitrary + correlated,
+    debug=True,
+    constant_treshold=0.99,
+    missing_treshold=0.94,
 )
 
-# Bundle preprocessing for numerical and categorical data
-preprocessor = ColumnTransformer(
-    transformers=[
-        ("numerical", numerical_transformer, numerical_cols),
-        ("categorical", categorical_transformer, cardinal_categorical_cols),
-    ]
+features = continuous + categorical
+all_features = features + ["SalePrice"]
+
+X[features].head()
+# %% [code]
+## Create pipeline
+continuous_transformer = make_pipeline(SimpleImputer(strategy="constant"))
+
+categorical_transformer = make_pipeline(
+    SimpleImputer(strategy="most_frequent"),
+    OneHotEncoder(handle_unknown="ignore"),
+)
+
+preprocessor = make_column_transformer(
+    (
+        continuous_transformer,
+        continuous,
+    ),
+    (
+        categorical_transformer,
+        categorical,
+    ),
+)
+
+estimator = XGBRegressor(n_estimators=100, learning_rate=0.1, random_state=0)
+
+pipeline = make_pipeline(preprocessor, estimator)
+
+score = get_score(pipeline, X, y, {"scoring": "neg_mean_absolute_error", "cv": 5})
+
+"{:,.0f}".format(score * -1)
+# %%
+# %% [code]
+## Search for algorithm
+look_for_algorithm(
+    X,
+    y,
+    enabled=False,
+    ratio=0.5,
+    preprocessor=preprocessor,
+    cv_options={"cv": 2, "scoring": "neg_mean_absolute_error"},
 )
 
 
-# 3. Cross-validation
-########################################
-
-model_configuration = {"n_estimators": 100, "learning_rate": 0.1, "random_state": 0}
-validation_range = np.arange(0.05, 0.3, 0.05)
-validation_argument = "learning_rate"
-validate = False
-submit = True
-
-
-def get_score(validation_configuration):
-    start_time = time.time()
-    print("Getting score for", validation_configuration)
-
-    pipeline = Pipeline(
-        steps=[
-            ("preprocessor", preprocessor),
-            (
-                "model",
-                XGBRegressor(**model_configuration, **validation_configuration),
-            ),
-        ]
-    )
-
-    scores = -1 * cross_val_score(
-        pipeline,
-        X,
-        y,
-        cv=5,
-        scoring="neg_mean_absolute_error",
-        fit_params={
-            # "model__early_stopping_rounds": 5,
-        },
-    )
-
-    mean = scores.mean()
-
-    end_time = time.time()
-    print("Scored", mean, "in", round(end_time - start_time), "s")
-
-    return mean
-
-
-# Evaluate the model
-if validate:
-    results = {
-        value: get_score({validation_argument: value}) for value in validation_range
-    }
-
-    plt.plot(list(results.keys()), list(results.values()))
-    plt.show()
-elif not submit:
-    get_score({})
-
-
-# 4. Testing
-########################################
-
-
-# Bundle preprocessing and modeling code in a pipeline
-pipeline = Pipeline(
-    steps=[
-        ("preprocessor", preprocessor),
-        (
-            "model",
-            XGBRegressor(**model_configuration),
-        ),
-    ]
-)
-
-# Preprocessing of training data, fit model
+# %% [code]
+## Train algorithm
 pipeline.fit(X, y)
 
-# Preprocessing of validation data, get predictions
-predictions = pipeline.predict(X_test)
 
-## Save test predictions to file
-output = pd.DataFrame({"Id": X_test.index, "SalePrice": predictions})
-output.to_csv("submission.csv", index=False)
+# %% [code]
+## Get predictions
+predictions = pipeline.predict(X_test)
+submissions = {"Id": X_test.index, "SalePrice": predictions}
+
+submit_predictions(submissions)
+
+# %%
